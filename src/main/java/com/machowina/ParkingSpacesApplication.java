ddef9f463f83c0c4f7d47c6952053d045d44591b
machowina <k.machowina@gmail.com>
2018-03-09 20:16:44 +0100

    Creating project

---
+++ src/main/java/com/machowina/ParkingSpacesApplication.java
@@ -0,0 +1,12 @@
+package com.machowina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSpacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSpacesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/machowina/ParkingSpacesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.machowina;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkingSpacesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
